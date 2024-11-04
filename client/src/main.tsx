cb2816b57deb2d4dddb04bddf8a074ba1061c17f
rickr1022 <info@firepie.com>
2024-11-03 22:22:31 -0800

    fix: Frontend UI setup with Tailwind CSS and styling improvements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.css'
+import './index.css'
 
-// Required for Solana wallets
-window.global ||= window;
-
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
